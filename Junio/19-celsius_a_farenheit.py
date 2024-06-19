53cdc28fd8cfd1689c2bf8d62021b8b606c41589
Gabriela <119881768+gabigonzalz@users.noreply.github.com>
2024-06-19 19:26:41 -0400

    19/06/24 Celsius a Fahrenheit

---
+++ Junio/19-celsius_a_farenheit.py
@@ -0,0 +1,5 @@
+# Desafio diario
+# COnvertir de Celsius a farenheit
+celsius = float(input("Introduce la temperatura en Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"{celsius} `C grados Celsius son {fahrenheit} `F grados Fahrenheit.")
